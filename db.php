7d0bc98da3e4d245ed157bc61bebf055d0332610
vivianmunguia <andy_munguia@hotmail.com>
2023-05-23 18:21:50 -0700

    The conection to the database has been created

---
+++ db.php
@@ -0,0 +1,12 @@
+<?php
+$conn = mysqli_connect( //Se utiliza la libreria mysqli y luego el metodo connect
+    'localhost', //Donde esta el servidor de la base de de datos
+    'root', //Usuario
+    '', //Contrasena, por defecto no tiene
+    'php_mysql_crud' //Base de datos a la que se conectara
+); 
+
+/*if (isset($conn)) { //Verifica que la variable $conn este definida y no sea nula
+    echo 'DB is connected';
+}*/
+?>

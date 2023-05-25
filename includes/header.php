dbb3cc4aba242f103ffa6a746d41d2ec55839b7e
vivianmunguia <andy_munguia@hotmail.com>
2023-05-24 19:49:19 -0700

    The tasks can be saved now

--- includes/header.php
+++ includes/header.php
@@ -9,3 +9,9 @@
     <title>PHP MYSQL CRUD</title>
 </head>
 <body>
+
+<nav class="navbar navbar-dark bg-dark">
+    <div class="container">
+        <a href="index.php" class="navbar-brand">PHP MYSQL CRUD</a>
+    </div>
+</nav>
---
+++ save_task.php
@@ -0,0 +1,15 @@
+<?php
+include("db.php");
+if (isset($_POST['save_task'])) {
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+    
+    $query = "INSERT INTO task(title, description) VALUES ('$title', '$description')";
+    $result = mysqli_query($conn, $query);
+    if (!$result) {
+        die("Query failed");
+    }
+    
+    header("Location: index.php");
+}
+?>

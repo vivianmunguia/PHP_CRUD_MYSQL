3868398c92dda902142ed25b5ec823d7b4a9387f
vivianmunguia <andy_munguia@hotmail.com>
2023-05-26 02:17:11 -0700

    Two buttons have been added to edit and delete the tasks

--- index.php
+++ index.php
@@ -41,7 +41,14 @@
                                     <td><?php echo $row['title'] ?></td>
                                     <td><?php echo $row['description'] ?></td>
                                     <td><?php echo $row['created_at'] ?></td>
-                                    <td></td>
+                                    <td>
+                                        <a href="edit.php?id=<?php echo $row['id']?>" class="btn btn-secondary">
+                                            <i class="fas fa-marker"></i>
+                                        </a>
+                                        <a href="delete_task.php?id=<?php echo $row['id']?>" class="btn btn-danger">
+                                            <i class="far fa-trash-alt"></i>
+                                        </a>
+                                    </td>
                                 </tr>
                             <?php } ?>
                     </tbody>
